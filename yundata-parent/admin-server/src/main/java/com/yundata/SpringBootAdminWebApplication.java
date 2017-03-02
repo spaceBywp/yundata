a3e1f8ea4e3b83af0214685e84052587336bf380
wangpeng <wangpeng@looktm.com>
2017-03-02 14:16:36 +0800

    添加服务存活监控 spring admin

---
+++ yundata-parent/admin-server/src/main/java/com/yundata/SpringBootAdminWebApplication.java
@@ -0,0 +1,18 @@
+package com.yundata;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 2 on 2017/3/2.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminWebApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringBootAdminWebApplication.class).web(true).run(args);
+    }
+}
